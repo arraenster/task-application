72e99240560012953b2327ff8b34522a02d9078c
arraenster <arraenster@gmail.com>
2023-03-24 13:03:56 +0100

    Customize error response

--- src/routers/user.js
+++ src/routers/user.js
@@ -121,6 +121,8 @@ const upload = new multer({
 
 router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
     res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({error: error.message})
 })
 
 module.exports = router
